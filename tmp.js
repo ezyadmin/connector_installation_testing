3a502251e6008cb7186f54d943e9dae8186a34fc
Wirasak Moukrong <test1@test1@test1>
2020-06-26 16:36:30 +0700

    init

---
+++ tmp.js
@@ -0,0 +1,16 @@
+let t = {
+  ez_config: [
+    {
+      conn_id: '{{connector_id_1}}',
+      module: '{{module_name_1}}',
+      cmd: '{{command_line_1}}',
+      regex: '{{regex_pattern_1}}',
+    },
+    {
+      conn_id: '{{connector_id_2}}',
+      module: '{{module_name_2}}',
+      cmd: '{{command_line_2}}',
+      regex: '{{regex_pattern_2}}',
+    },
+  ],
+}
